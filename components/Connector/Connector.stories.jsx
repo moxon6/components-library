f01dfaa19d6e9837cb68c5bee2d96c7a19f651e8
Martin Moxon <moxon6@googlemail.com>
2020-01-02 19:57:45 +0000

    Added Connector.stories.scss

--- components/Connector/Connector.stories.jsx
+++ components/Connector/Connector.stories.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import events from './events';
 
 import Connector from './Connector';
+import { withKnobs, button } from "@storybook/addon-knobs";
+import './Connector.stories.scss';
 
 export default {
   title: 'Connector',
+  decorators: [withKnobs]
 };
 
-export const connectorExample = () => <Connector events={events} />
+button("Dispatch scroll event", () => {
+  events.dispatchEvent("content.scroll")
+});
 
+
+export const connectorExample = () => (
+  <div className="connector-container">
+    <Connector events={events} />
+  </div>
+)
