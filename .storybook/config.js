970535d956859b173ba591ce1fdb7881c90b3b9f
Martin Moxon <moxon6@googlemail.com>
2020-01-04 22:32:17 +0000

    Change to light theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
 
+// Option defaults.
+addParameters({
+  options: {
+    theme: themes.light,
+  },
+});
 // automatically import all files ending in *.stories.js
 configure(require.context('../components', true, /\.stories\.js.?$/), module);
