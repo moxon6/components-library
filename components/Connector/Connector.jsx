8d06cb31a9cb0dc6199179c6d45158a0b1245164
Martin Moxon <moxon6@googlemail.com>
2020-01-04 13:38:53 +0000

    Convert to sass module, make parameterisable

--- components/Connector/Connector.jsx
+++ components/Connector/Connector.jsx
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect, useRef, useState } from 'react';
-import './Connector.scss';
+import defaultStyles from './Connector.module.scss';
 import classNames from 'classnames';
 
 const createViewBox = (width, height) => (
@@ -15,7 +15,7 @@ const createPath = (width, height) => (
     `
 )
 
-export default function Connector({ isVisible }) {
+export default function Connector({ isVisible, styles }) {
 
     const containerRef = useRef(null);
     const [pathLength, setPathLength] = useState(0);
@@ -35,18 +35,25 @@ export default function Connector({ isVisible }) {
     }, [])
 
     return (
-        <div className="line-container-container">
+        <div className={styles.lineContainerContainer}>
             <div className={classNames({
-                "line-container": true,
-                "hidden": !isVisible
+                [styles.lineContainer]: true,
+                [styles.hidden]: !isVisible
             })} ref={containerRef}>
-                <svg style={{ '--pathLength': pathLength}} className="connector-line" version="1.1" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg" viewBox={createViewBox(dimensions.width, dimensions.height)} height="100%" width="100%">
-                    <path d={createPath(dimensions.width, dimensions.height)} vectorEffect="non-scaling-stroke" strokeWidth="2"
-                        fill="none">
+                <svg style={{ '--pathLength': pathLength}} className={styles.connectorLine} version="1.1" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg" viewBox={createViewBox(dimensions.width, dimensions.height)} height="100%" width="100%">
+                    <path d={createPath(dimensions.width, dimensions.height)} vectorEffect="non-scaling-stroke" strokeWidth="2" fill="none">
                     </path>
                 </svg>
 
             </div>
         </div>
     )
+}
+
+Connector.propTypes = {
+
+}
+
+Connector.defaultProps = {
+    styles: defaultStyles
 }
